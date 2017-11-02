340f639242666aaab861ba6811a320edb6b9636c
greyjacket <asch716@gmail.com>
2017-11-02 17:16:31 -0400

    Second Revisions

--- map_sizer.py
+++ map_sizer.py
@@ -1,6 +1,80 @@
 #!/usr/bin/python
-import csv, sys, math
+import csv, sys, math, operator
 from utils import get_aspect_ratio, calculate_price
+
+def calculate_dimensions(size, orientation):
+			item_size = {}
+
+			if(orientation == 0):
+				size2 = size * (ratio)
+			else:
+				size2 = size * (1.0/ratio)
+
+			# this function returns of tuple containing the fractional and integral part of the real number
+			size2_split = math.modf(size2)
+			decimal_part = size2_split[0]
+				
+			# round up from .3
+			if decimal_part >= .3:
+				size2 = math.ceil(size2)
+			else:
+				size2 = math.floor(size2)
+
+			if size2 >= 15.0 and size2 < 16.0:
+				size2 = 16.0
+			if size2 >= 17.0 and size2 < 18.0:
+				size2 = 16.0
+			if size2 >= 19.0 and size2 < 20.0:
+				size2 = 18.0			
+
+			if(orientation == 0):
+				height = size
+				width = size2
+			else:
+				height = size2
+				width = size
+
+
+			# set the square inches
+			square_inches = height * width
+			if square_inches > 240 and square_inches <= 2400:
+
+				price = calculate_price(square_inches)
+
+				# get the string value
+				height_str = str(height)
+				width_str = str(width)
+
+				int_str1 = str(int(width))
+				int_str2 = str(int(height))
+
+				unique1 = int_str1
+				unique2 = int_str2
+
+				width_int_str = str(int(width))
+				height_int_str = str(int(height))
+
+				# pad a single digit with a zero if need be
+				if len(width_int_str) < 2:
+					unique1 = "0" + int_str1
+				if len(height_int_str) < 2:
+					unique2 = "0" + int_str2
+
+				# create the unique sku
+				unique = unique1 + unique2
+				unique_sku = sku + "_" + unique
+
+				# create the size name
+				size_name = width_int_str + "in" + " x " + height_int_str + "in"
+				
+				item_size['Height'] = height_str
+				item_size['Width'] = width_str
+				item_size['SqIn'] = square_inches
+				item_size['SizeName'] = size_name
+				item_size['UniqueSku'] = unique_sku
+				item_size['Price'] = price
+
+				return item_size
 
 try:
 	filename = sys.argv[1]
@@ -35,6 +109,7 @@ for item in newCsv:
 	image_width = float(item['ImageWidth'])
 	image_height = float(item['ImageHeight'])
 	sku = item['Sku']
+	item_sizes = []
 
 	# keep the aspect ratio >= 1
 	if image_width >= image_height:
@@ -59,68 +134,42 @@ for item in newCsv:
 		polarity = 0
 
 	while (polarity < 2):
-
-		for size in sizes:		
-		
-			if(polarity == 0):
-				size2 = size * (ratio)
-			else:
-				size2 = size * (1.0/ratio)
-
-			# this function returns of tuple containing the fractional and integral part of the real number
-			size2_split = math.modf(size2)
-			decimal_part = size2_split[0]
-				
-			# round up from .3
-			if decimal_part >= .3:
-				size2 = math.ceil(size2)
-			else:
-				size2 = math.floor(size2)
-
-			height = size
-			width = size2
-
-			# set the square inches
-			square_inches = height * width
-
-			if square_inches > 240 and square_inches <= 2400:
-
-				price = calculate_price(square_inches)
-
-				# get the string value
-				height_str = str(height)
-				width_str = str(width)
-
-				int_str1 = str(int(width))
-				int_str2 = str(int(height))
-
-				unique1 = int_str1
-				unique2 = int_str2
-
-				width_int_str = str(int(width))
-				height_int_str = str(int(height))
-
-				# pad a single digit with a zero if need be
-				if len(width_int_str) < 2:
-					unique1 = "0" + int_str1
-				if len(height_int_str) < 2:
-					unique2 = "0" + int_str2
-
-				# create the unique sku
-				unique = unique1 + unique2
-				unique_sku = sku + "_" + unique
-
-				# create the size name
-				size_name = width_int_str + "in" + " x " + height_int_str + "in"
-				
-				properties_list.append(height_str)
-				properties_list.append(width_str)
-				properties_list.append(str(square_inches))
-				properties_list.append(size_name)
-				properties_list.append(unique_sku)
-				properties_list.append(str(price))
-			
+		item_size = calculate_dimensions(24, polarity)
+		if item_size:		
+			item_sizes.append(item_size)
 		polarity+=1
+	
+	item_size = calculate_dimensions(44, 1)
+
+	if item_size:	
+		newItem = {}
+		for item in item_sizes:
+			square_inches1 = item['SqIn']
+			square_inches2 = item_size['SqIn']
+
+			if square_inches1 >= square_inches2:
+				square_ratio = square_inches1/square_inches2
+			else:
+				square_ratio = square_inches2/square_inches1
+			if square_ratio < ratio:
+				newItem = calculate_dimensions(36, 0)
+				break
+			else:
+				newItem = item_size
+
+		if newItem:
+			item_sizes.append(newItem)
+
+	item_sizes.sort(key=operator.itemgetter('SqIn'))
+	
+	for item in item_sizes:
+		properties_list.append(item['Height'])
+		properties_list.append(item['Width'])
+		properties_list.append(str(item['SqIn']))
+		properties_list.append(item['SizeName'])
+		properties_list.append(item['UniqueSku'])
+		properties_list.append(str(item['Price']))
+
 
 	write_tuple = (sku, image_height, image_width)
 
